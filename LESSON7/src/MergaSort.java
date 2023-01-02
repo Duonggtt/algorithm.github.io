1832cb9410311bc1da8f99474497d17a315f3dda
Duonggtung <duonggtung173@gmail.com>
2023-01-02 20:28:20 +0700

    BTVN

--- LESSON7/src/MergaSort.java
+++ LESSON7/src/MergaSort.java
@@ -3,48 +3,58 @@ import java.util.Arrays;
 public class MergaSort {
 
     public static void main(String[] args) {
-        int[] a = {9,5,2,3,1,6,7,4};
-        int[] c = new int[a.length];
-        int start = 0;
-        int end = a.length-1;
+        int[] c = {9,5,2,3,1,6,7,4};
         //============================================
-        System.out.println(Arrays.toString(mergaSort(c, a, start, end)));
+        mergaSort(c,0,c.length-1);
+        for (int i =0;i<c.length;i++) {
+            System.out.print(c[i] + " ");
+        }
     }
 
-    public static int[] mergaSort(int[] c,int[] a,int start,int end) {
-        if(start >= end ) {
-            return  a;
+    public static void mergaSort(int[] c,int start,int end) {
+        if(start < end) {
+            int mid = (start + end) / 2;
+            mergaSort(c,start,mid);
+            mergaSort(c,mid + 1,end);
+            merga(c,start,mid,end);
         }
-        int mid = (start + end) / 2;
-        mergaSort(c,a,start,mid);
-        mergaSort(c,a,mid + 1,end);
-        int i=start;
-        int j=mid+1;
-        int u=start;
-        while(i <= mid && j <= end) {
-            if(a[i] > a[j]){
-                c[u] = a[j];
-                j++;
-                u++;
+    }
+
+    public static int[] merga(int[] c,int start,int mid,int end) {
+        int aLength = mid - start + 1;
+        int bLength = end - mid;
+
+        int[] a = new int[aLength];
+        int[] b = new int[bLength];
+        for(int i =0;i<aLength;i++) {
+            a[i] = c[start + i];
+        }
+        for(int i =0;i<aLength;i++) {
+            b[i] = c[mid + 1 + i];
+        }
+
+        int i=0;
+        int j=0;
+        int u = start;
+        while(i <aLength && j <bLength) {
+            if(a[i] <= b[j]) {
+                c[u] = a[i];
+                i++;
             }else {
-                c[u] = a[i];
-                u++;
-                i++;
-            }
-        }
-        if(i<=mid) {
-            while(i<=mid) {
-                c[u] = a[i];
-                i++;
-                u++;
-            }
-        }
-        if(j <= end) {
-            while(j <= end) {
-                c[u] = a[j];
+                c[u] = b[j];
                 j++;
-                u++;
             }
+            u++;
+        }
+        while(i < aLength) {
+            c[u] = a[i];
+            i++;
+            u++;
+        }
+        while(j < bLength) {
+            c[u] = b[j];
+            j++;
+            u++;
         }
         return c;
     }
